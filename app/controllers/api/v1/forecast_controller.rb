5cf9f710c4b6cc19ebbf777b95eef9e73283e048
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-18 10:21:23 -0400

    Refactor index method to use GeocodingFacade

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,13 +2,8 @@ class Api::V1::ForecastController < ApplicationController
   def index
     location = forecast_params[:location]
 
-    conn = Faraday.new('http://www.mapquestapi.com')
-
-    response = conn.get("/geocoding/v1/address?key=#{ENV['MAPQUEST_API_KEY']}&location=#{location}")
-
-    json = JSON.parse(response.body, symbolize_names: true)
-
-    lat_long = json[:results].first[:locations].first[:latLng]
+    geocoding_facade = GeocodingFacade.new(location)
+    lat_long = geocoding_facade.find_lat_long
   end
 
   def forecast_params
