aaa0fc4cbe5d5990ae8825146437f53b9f5241ff
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-18 09:23:38 -0400

    Add strong params: forecast_params

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::ForecastController < ApplicationController
   def index
     require "pry"; binding.pry
+    
+  end
+
+  def forecast_params
+    params.permit(:location)
   end
 end
