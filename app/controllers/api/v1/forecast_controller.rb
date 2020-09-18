fc345c4b029d2b6bbd15508ec3379c967161f868
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-18 09:35:21 -0400

    Add faraday

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::ForecastController < ApplicationController
   def index
     location = forecast_params[:location]
-    
+
+    conn = Faraday.new('http://www.mapquestapi.com')
+
+    require "pry"; binding.pry
   end
 
   def forecast_params
