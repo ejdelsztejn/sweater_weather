e3882b5ff184bf748cd8baf8f8c20eaaeecfbef3
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-18 09:13:14 -0400

    Add forecast_spec

---
+++ spec/requests/api/v1/forecast_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'Forecast Endpoints' do
+  describe 'As a user, when I visit the landing page,' do
+    it 'a get request is made to retrieve a JSON response with data related to the weather in Denver' do
+      create(:forecast)
+
+      get '/api/v1/forecast?location=denver,co'
+      expect(response).to be_successful
+      expect(response.content_type).to eq("application/json")
+
+      forecast = JSON.parse(response.body, symbolize_names: true)
+
+      # expect(forecast[:results]
+    end
+  end
+end
