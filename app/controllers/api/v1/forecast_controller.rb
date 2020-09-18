e85826e839ebe26067efd0eea16d5d94d9b7aeab
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-18 09:37:38 -0400

    Get response

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::ForecastController < ApplicationController
 
     conn = Faraday.new('http://www.mapquestapi.com')
 
+    response = conn.get("/geocoding/v1/address?key=#{ENV['MAPQUEST_API_KEY']}&location=#{location}")
     require "pry"; binding.pry
   end
 
