b830568ca2543eb620f2ff18178f3b6c7bde7b0e
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-21 11:11:35 -0400

    Addd attributes to DailyForecast poro

--- spec/poros/daily_forecast_spec.rb
+++ spec/poros/daily_forecast_spec.rb
@@ -14,17 +14,17 @@ describe DailyForecast do
         main: 'Rain'
     }]
   }
+
+    daily_forecast = DailyForecast.new(attrs)
+
+    expect(daily_forecast).to be_a DailyForecast
+    expect(daily_forecast.dt).to eq(1600692843)
+    expect(daily_forecast.temp).to eq(299)
+    expect(daily_forecast.min_temp).to eq(289)
+    expect(daily_forecast.max_temp).to eq(303)
+    expect(daily_forecast.weather_condition).to eq('Rain')
+    expect(daily_forecast.rain).to eq(0.12)
+    expect(daily_forecast.snow).to eq(0)
+    expect(daily_forecast.precipitation).to eq(0.12)
   end
-
-  daily_forecast = DailyForecast.new(attrs)
-
-  expect(daily_forecast).to be_a DailyForecast
-  expect(daily_forecast.dt).to eq(1600692843)
-  expect(daily_forecast.temp).to eq(298)
-  expect(daily_forecast.min_temp).to eq(289)
-  expect(daily_forecast.max_temp).to eq(302)
-  expect(daily_forecast.weather_condition).to eq('Rain')
-  expect(daily_forecast.rain).to eq(0.12)
-  expect(daily_forecast.snow).to eq(0)
-  expect(daily_forecast.precipitation).to eq(0.12)
 end
