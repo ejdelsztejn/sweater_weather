eb64f7ad768bf4297ee9755229a1b00273f22cf8
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-22 16:21:41 -0400

    Make pixabay call

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::BackgroundsController < ApplicationController
   def index
+    conn = Faraday.new("https://pixabay.com/api") do |req|
+      req.params["key"] = ENV["PIXABAY_API_KEY"]
+    end
+    response = conn.get do |req|
+      req.params[:q] = background_params[:location]
+    end
+    require "pry"; binding.pry
   end
 
   private
