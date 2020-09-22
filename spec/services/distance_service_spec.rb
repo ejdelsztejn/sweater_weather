81f458cc431c833088da418a8341be5f8ea9f279
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-22 13:44:53 -0400

    Final commit

--- spec/services/distance_service_spec.rb
+++ spec/services/distance_service_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe DistanceService do
 
     expect(results).to be_a(String)
   end
+
+  it 'can return the realtime value for time' do
+    distance_service = DistanceService.new
+    results = distance_service.get_realtime('denver,co', 'pueblo,co')
+
+    expect(results).to be_a(Integer)
+  end
 end
