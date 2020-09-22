abd468be711664ab43f0744d9f6eaaea26ae67f7
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-22 15:43:46 -0400

    Implement VCR for testing API calls

--- spec/services/geocoding_service_spec.rb
+++ spec/services/geocoding_service_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'GeocodingService' do
   it 'can return data by location' do
-    location_service = GeocodingService.new
-    results = location_service.get_location_data('denver,co')
+    VCR.use_cassette('geocoding_service') do
+      location_service = GeocodingService.new
+      results = location_service.get_location_data('denver,co')
 
-    expect(results).to be_a(Hash)
+      expect(results).to be_a(Hash)
+    end
   end
 end
