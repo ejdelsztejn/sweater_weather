9fe779811e32308ae7b2ec5163b0ebe69f1b0281
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-22 11:27:54 -0400

    Make API call to MapQuest

--- app/controllers/api/v1/foodie_controller.rb
+++ app/controllers/api/v1/foodie_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::FoodieController < ApplicationController
   def index
-  
+    conn = Faraday.new(url: 'http://www.mapquestapi.com')
+    response = conn.get("/directions/v2/route?key=#{ENV['MAPQUEST_API_KEY']}&from=#{foodie_params[:start]}&to=#{foodie_params[:end]}")
+    json = JSON.parse(response.body, symbolize_names: true)
   end
 
   private
