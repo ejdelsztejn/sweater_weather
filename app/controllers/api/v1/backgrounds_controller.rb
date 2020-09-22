bda8468ccdca2d3400684f29d3600cffb88642fa
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-22 16:59:23 -0400

    Create PixabayService

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -2,14 +2,6 @@ class Api::V1::BackgroundsController < ApplicationController
   def index
     city = background_params[:location].split(',')[0]
 
-    conn = Faraday.new("https://pixabay.com/api/") do |req|
-      req.params["key"] = ENV["PIXABAY_API_KEY"]
-    end
-    response = conn.get do |req|
-      req.params[:q] = city
-    end
-    json = JSON.parse(response.body, symbolize_names: true)
-    require "pry"; binding.pry
   end
 
   private
