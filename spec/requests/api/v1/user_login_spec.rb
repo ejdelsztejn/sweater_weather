c243339f2e8b8113cddb26e8fb66e49a88609358
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-23 12:38:43 -0400

    Add user login sad path test

--- spec/requests/api/v1/user_login_spec.rb
+++ spec/requests/api/v1/user_login_spec.rb
@@ -36,5 +36,29 @@ RSpec.describe 'Log In a User' do
       expect(json[:data][:attributes]).to_not include(:password)
       expect(json[:data][:attributes]).to_not include(:password_digest)
     end
+    it 'if user email cannot be found, an error is thrown' do
+      headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+
+      params = {
+        "email": "whomever@example.com",
+        "password": "password123"
+      }
+
+      post '/api/v1/sessions', headers: headers, params: JSON.generate(params)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(400)
+      expect(response.content_type).to include("application/json")
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(json).to_not have_key(:data)
+      expect(json).to have_key(:error)
+      expect(json[:error]).to have_key(:message)
+      expect(json[:error][:message]).to eq('Bad credentials')
+    end
   end
 end
