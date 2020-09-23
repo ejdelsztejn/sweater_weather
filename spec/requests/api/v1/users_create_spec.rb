1c8c4e7120d5b4a2d7db3f9dc6805923b4b1b974
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-23 11:00:18 -0400

    Add sad path test for missing email field

--- spec/requests/api/v1/users_create_spec.rb
+++ spec/requests/api/v1/users_create_spec.rb
@@ -6,35 +6,58 @@ RSpec.describe 'Create a User' do
       User.destroy_all
     end
     it 'I can register as a user' do
-    headers = {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    }
+      headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
 
-    params = {
-      "email": "whatever@example.com",
-      "password": "password",
-      "password_confirmation": "password"
-    }
+      params = {
+        "email": "whatever@example.com",
+        "password": "password",
+        "password_confirmation": "password"
+      }
 
-    post '/api/v1/users', headers: headers, params: JSON.generate(params)
+      post '/api/v1/users', headers: headers, params: JSON.generate(params)
 
-    expect(response).to be_successful
-    expect(response.content_type).to include("application/json")
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+      expect(response.content_type).to include("application/json")
 
-    json = JSON.parse(response.body, symbolize_names: true)
+      json = JSON.parse(response.body, symbolize_names: true)
 
-    expect(json).to have_key(:data)
-    expect(json[:data]).to have_key(:id)
-    expect(json[:data][:id]).to_not eq(nil)
-    expect(json[:data][:id]).to be_a(String)
-    expect(json[:data]).to have_key(:type)
-    expect(json[:data][:type]).to eq('users')
-    expect(json[:data]).to have_key(:attributes)
-    expect(json[:data][:attributes]).to include(:email)
-    expect(json[:data][:attributes]).to include(:api_key)
-    expect(json[:data][:attributes]).to_not include(:password)
-    expect(json[:data][:attributes]).to_not include(:password_digest)
+      expect(json).to have_key(:data)
+      expect(json[:data]).to have_key(:id)
+      expect(json[:data][:id]).to_not eq(nil)
+      expect(json[:data][:id]).to be_a(String)
+      expect(json[:data]).to have_key(:type)
+      expect(json[:data][:type]).to eq('users')
+      expect(json[:data]).to have_key(:attributes)
+      expect(json[:data][:attributes]).to include(:email)
+      expect(json[:data][:attributes]).to include(:api_key)
+      expect(json[:data][:attributes]).to_not include(:password)
+      expect(json[:data][:attributes]).to_not include(:password_digest)
+    end
+    it 'if email field is blank, an error is thrown' do
+      headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+
+      params = {
+        "email": "",
+        "password": "password",
+        "password_confirmation": "password"
+      }
+
+      post '/api/v1/users', headers: headers, params: JSON.generate(params)
+
+      expect(response.status).to eq(400)
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(json).to_not have_key(:data)
+      expect(json).to have_key(:error)
+      expect(json[:error]).to eq('Email address is required')
     end
   end
 end
