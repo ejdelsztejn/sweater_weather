07cb7cd1101e298a96a9ca0166a4a018508eaf16
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-23 12:34:24 -0400

    Pass happy path for user login:

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,3 +1,12 @@
 class Api::V1::SessionsController < ApplicationController
-  
+  def create
+    user = User.find_by(email: session_params[:email])
+    if user && user.authenticate(params[:password])
+      render json: UsersSerializer.new(user), status: 200
+    end
+  end
+
+  def session_params
+    params.permit(:email, :password)
+  end
 end
