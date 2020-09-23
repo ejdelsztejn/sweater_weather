1d184afd64979a2bfbb07713ee14b042d42f133c
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-23 10:25:21 -0400

    Add test user api key should not be nil:

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'callbacks' do
+    it 'will have an api_key automatically assigned when created' do
+      user = User.create!(
+        email: 'email',
+        password: '123',
+        password_confirmation: '123'
+      )
+      expect(user.api_key).to_not be_nil
+    end
+  end
 end
