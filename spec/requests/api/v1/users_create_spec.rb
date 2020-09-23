798951dcc2d5e976b9541230ac7420f9fa6820b0
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-23 07:21:38 -0400

    Add user create spec

---
+++ spec/requests/api/v1/users_create_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Create a User' do
+  describe 'As a visitor,' do
+    it 'I can register as a user' do
+    headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+
+    params = {
+      "email": "whatever@example.com",
+      "password": "password",
+      "password_confirmation": "password"
+    }
+
+    post '/api/v1/users', headers: headers, params: params
+
+    expect(response).to be_successful
+    expect(response.content_type).to include("application/json")
+    end
+  end
+end
